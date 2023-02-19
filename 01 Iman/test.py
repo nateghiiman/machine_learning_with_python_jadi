064514064b94ef2158dc4bb5214f61556306e4d3
nateghiiman <nateghiiman@gmail.com>
2023-02-19 19:33:57 +0330

    Chapter3 projects done

---
+++ 01 Iman/test.py
@@ -0,0 +1,11 @@
+
+from sklearn.metrics import jaccard_score
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+#Predicted = [float(1)]
+#Actual =    [0, 1, 0, 0, 1, 1, 1]
+array = np.array([1, 2, 3], dtype=float) 
+#print(jaccard_score(Predicted,Actual))
+from sklearn.neighbors import KNeighborsClassifier
+print("8")
+print(np.version.version)
